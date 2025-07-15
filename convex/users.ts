a56189eb4ab377217d99ffb40e8926b23873d4f9
sivasankar55 <sivasankar8875@gmail.com>
2025-07-15 12:55:38 +0530

    created 2 query

--- convex/users.ts
+++ convex/users.ts
@@ -1,5 +1,5 @@
 import {v} from "convex/values";
-import { mutation } from "./_generated/server";
+import { mutation,query } from "./_generated/server";
 
 
 export const syncUser = mutation({
@@ -20,4 +20,24 @@ export const syncUser = mutation({
     });
  },
     
+});
+
+export const getUsers = query({
+    handler: async(ctx) => {
+        const identity = await ctx.auth.getUserIdentity();
+        if(!identity) throw new Error("User is not authenticated");
+
+        const users = await ctx.db.query("users").collect();
+
+        return users;
+    },
+});
+
+export const getUserByClerkId = query({
+    args: {clerkId: v.string()},
+    handler: async(ctx,args) => {
+        const user = await ctx.db.query("users").withIndex("by_clerk_id", (q) => q.eq("clerkId",args.clerkId)).first();
+
+        return user;
+    },
 });
