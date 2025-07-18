8abc164ef7e80b42f0d020b2ff940e63d90199ee
sivasankar55 <sivasankar8875@gmail.com>
2025-07-18 21:43:37 +0530

    stream wrap to root

---
+++ src/app/(root)/layout.tsx
@@ -0,0 +1,9 @@
+import StreamVideoProvider from "@/components/ui/providers/StreamClientProvider";
+
+
+function Layout({children}: {children: React.ReactNode}) {
+    return <StreamVideoProvider>{children}</StreamVideoProvider>;
+
+}
+
+export default Layout;
